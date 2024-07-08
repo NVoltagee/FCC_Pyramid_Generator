7deb581e07866a6ecd04e26a7c5ff021e1bc65e8
NVoltagee <natrujillo9@gmail.com>
2024-07-08 19:06:53 -0400

    - Commented out a for loop to create rows with centered characters - Added a while loop to create rows using padRow function until rows length is less than count - Added a for loop to create rows in reverse order using padRow function

--- script.js
+++ script.js
@@ -12,9 +12,17 @@ function padRow(rowNumber, rowCount) {
   );
 }
 
-for (let i = 1; i <= count; i = i ++ 1) {
-  rows.push(padRow(i, count));
-}
+// for (let i = 1; i <= count; i = i ++ 1) {
+//   rows.push(padRow(i, count));
+// }
+
+// while (rows.length < count) {
+//   rows.push(padRow(rows.length + 1, count));
+// }
+
+// for (let i = count; i > 0; i--) {
+//   rows.push(padRow(i, count));
+// }
 
 let result = "";
 console.log(result);
